29421462b3b426fd153921bc7d1526295d91c245
Kwaku Antwi <kwaku@dodec.co.uk>
2014-01-02 13:12:24 +0000

    installed new files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MoneyTr::Application.config.secret_key_base = '528c8e14ceebb204e69677f597ba03b355adc9050a1d6fce526e9e76ac7eb0746f5d4fb204861644ab8eb698bc9421778f7c94f809d182b4aa5603e5c8046cfd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MoneyTr::Application.config.session_store :cookie_store, key: '_money_tr_session'
